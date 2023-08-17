8e451fe968bd4c68ce608bdd9be356664cbfa2e0
Daniel Munro <danbmunro@gmail.com>
2023-08-17 17:00:42 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # latent-rna
  Extract latent transcriptomic phenotypes
+
+## Usage
+
+### RNA-seq coverage counts
+
+
+
+### Generating latent phenotypes
+
+Depending on the size of the dataset and usage needs, this same method can be run in different ways. First:
+
+- The per-gene PCA models can be computed and saved, then applied to the same or other datasets. Applying the same set of models to multiple datasets allows the features to correspond across datasets.
+- Each gene can be fit and transformed in one step, so that the input data is only loaded once and models aren't saved.
+
+Second:
+
+- For small datasets, all per-sample coverage files can be loaded directly, assembled and split by gene, and processed in sequence.
+- For datasets where the coverage counts can't all be loaded into memory at once, per-sample coverage files are first assembled in batches into per-gene tables and saved. Coverage counts can then be loaded and processed in sequence per gene.
+  <!-- - Or, the program can be run for a single gene, allowing genes to be processed in parallel using separate calls to the program. -->
+
+Thus, the four ways to run the procedure are:
+
+#### Small dataset, save and/or reuse models
+
+```shell
+python latent_RNA.py fit -i covgfiles.txt -r gene_bins.bed.gz -o models.pkl
+python latent_RNA.py transform -i covgfiles.txt -r gene_bins.bed.gz -m models.pkl -o phenos.tsv.gz
+```
+
+#### Small dataset, one-time transformation
+
+```shell
+python latent_RNA.py fit-transform -i covgfiles.txt -r gene_bins.bed.gz -o phenos.tsv.gz
+```
+
+#### Large dataset, save and/or reuse models
+
+```shell
+python latent_RNA.py prepare -i covgfiles.txt -r gene_bins.bed.gz -d gene_covg/
+python latent_RNA.py fit -d gene_covg/ -o models.pkl
+python latent_RNA.py transform -d gene_covg/ -m models.pkl -o phenos.tsv.gz
+```
+
+#### Large dataset, one-time transformation
+
+```shell
+python latent_RNA.py prepare -i covgfiles.txt -r gene_bins.bed.gz -d gene_covg/
+python latent_RNA.py fit-transform -d gene_covg/ -o phenos.tsv.gz
+```
+
+#### Multiple datasets
+
+To process multiple datasets, e.g. GTEx tissues, in a way that results in phenotypes that correspond across datasets, it is recommended to run the `prepare` step separately for each tissue, so that each dataset's per-gene coverage files can be used to run the `transform` step separately. The `fit` step can take a file listing multiple gene coverage directories, and will concatenate per gene to fit the models:
+
+```shell
+python latent_RNA.py prepare -i covgfiles.tissue1.txt -r gene_bins.bed.gz -d gene_covg_tissue1/
+python latent_RNA.py prepare -i covgfiles.tissue2.txt -r gene_bins.bed.gz -d gene_covg_tissue2/
+echo 'gene_covg_tissue1/\ngene_covg_tissue2/' > gene_covg_dirs.txt
+python latent_RNA.py fit --dirs gene_covg_dirs.txt -o models.pkl
+python latent_RNA.py transform -d gene_covg_tissue1/ -m models.pkl -o phenos_tissue1.tsv.gz
+python latent_RNA.py transform -d gene_covg_tissue2/ -m models.pkl -o phenos_tissue2.tsv.gz
+```
---
+++ latent_RNA.py
@@ -0,0 +1,227 @@
+"""Run PCA on feature bin coverage and create BED file"""
+
+import argparse
+from pathlib import Path
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+from tqdm import tqdm
+
+def norm_counts_over_gene(counts: pd.DataFrame) -> pd.DataFrame:
+    """For one gene, scale coverage per sample to have the same total per sample
+    
+    Scale each total to the median of the totals per sample, except for samples
+    with 0 total, which are set to 0. This is to prevent NaNs which would cause
+    the entire gene to be dropped if it had 0 expression in a single sample.
+    """
+    sample_totals = counts.sum(axis=0)
+    scale_factors = sample_totals.median() / sample_totals
+    scale_factors = scale_factors.fillna(0).replace(np.inf, 0)
+    counts = counts.mul(scale_factors, axis=1)
+    assert not counts.isna().any().any()
+    return counts
+
+def normalize_gene(df: pd.DataFrame, regions: pd.DataFrame) -> pd.DataFrame:
+    """Normalize coverage for one gene"""
+    # Normalize coverage to ignore total expression, i.e. each sample has same total coverage for the gene
+    df = norm_counts_over_gene(df)
+    # Normalize counts to mean coverage per bp within each region
+    df = df.div(regions['length'].loc[df.index], axis=0)
+    # Variance stabilization:
+    df = np.sqrt(df)
+    return df
+
+def load_regions(regionfile: Path) -> pd.DataFrame:
+    regions = pd.read_csv(regionfile, sep='\t', header=None, usecols=[1, 2, 3], names=['start', 'end', 'region'], index_col='region')
+    regions['length'] = regions['end'] - regions['start']
+    regions = regions.drop(['start', 'end'], axis=1)
+    # Split region label to get multiindex:
+    regions.index = regions.index.str.split('_', n=1, expand=True)
+    regions.index.set_names(['gene_id', 'start'], inplace=True)
+    return regions
+
+def load_inputs(infile: Path, regions, parse_ids: bool) -> pd.DataFrame:
+    """Load coverage files into one dataframe and normalize coverage"""
+    infiles = pd.read_csv(infile, header=None, names=['path'])
+    if parse_ids:
+        infiles['sample'] = [Path(p).name.split('.')[0] for p in infiles['path']]
+        assert infiles['sample'].is_unique
+    else:
+        infiles['sample'] = [f'S{i + 1}' for i in range(infiles.shape[0])]
+    print(f'Loading {len(infiles)} coverage files...', flush=True)
+    counts = np.zeros((regions.shape[0], infiles.shape[0]), dtype=int)
+    for i, fname in tqdm(enumerate(infiles['path']), total=len(infiles['path']), desc="Loading files"):
+        d = pd.read_csv(fname, header=None, dtype=int, names=['count'], index_col=False)
+        assert d.shape[0] == regions.shape[0]
+        counts[:, i] = d['count']
+    counts = pd.DataFrame(counts, index=regions.index, columns=infiles['sample'])
+    return counts
+
+def subset_pcs(pca: PCA, n_pcs_max: int):
+    """Subset the PCA model to n_pcs_max PCs to reduce file size"""
+    if n_pcs_max != 0 and n_pcs_max < pca.n_components_:
+        pca.components_ = pca.components_[:n_pcs_max, :]
+        pca.n_components_ = n_pcs_max
+        pca.explained_variance_ = pca.explained_variance_[:n_pcs_max]
+        pca.explained_variance_ratio_ = pca.explained_variance_ratio_[:n_pcs_max]
+        pca.singular_values_ = pca.singular_values_[:n_pcs_max]
+        pca.noise_variance_ = None # Could compute if necessary, but omitting otherwise
+
+def gene_fit(df: pd.DataFrame, regions: pd.DataFrame, var_expl: float, n_pcs_max: int) -> PCA:
+    """Fit PCA model to normalized coverage for one gene
+    
+    Saves enough PCs to explain var_expl variance or n_pcs_max, whichever is smaller.
+    """
+    df = normalize_gene(df, regions)
+    df = df.loc[df.std(axis=1) > 0, :]
+    if df.shape[0] == 0:
+        return None
+    x = df.values.T
+    # Center and scale the data:
+    xmean = x.mean(axis=0)
+    xstd = x.std(axis=0)
+    x = (x - xmean) / xstd
+    pca = PCA(n_components=var_expl) if var_expl not in {0, 1} else PCA()
+    pca.fit(x)
+    subset_pcs(pca, n_pcs_max)
+    features = pd.DataFrame(index=df.index) # Save feature stats for transforming new data
+    features['mean'] = xmean
+    features['std'] = xstd
+    return {'pca': pca, 'features': features}
+
+def gene_transform(df: pd.DataFrame, regions: pd.DataFrame, pca: PCA, features: pd.DataFrame) -> pd.DataFrame:
+    """Apply PCA model to normalized coverage for one gene"""
+    df = normalize_gene(df, regions)
+    # Filter df to include same features as the model:
+    df = df.loc[features.index, :]
+    x = df.values.T
+    # Center and scale the data using stats from the model-fitting data:
+    x = (x - features['mean'].values) / features['std'].values
+    mat = pca.transform(x).T
+    pc_names = [f'PC{i + 1}' for i in range(mat.shape[0])]
+    out = pd.DataFrame(mat, index=pc_names, columns=df.columns)
+    out.index.set_names('PC', inplace=True)
+    # Add gene_id to index:
+    out.reset_index(inplace=True)
+    out.insert(0, 'gene_id', df.index.get_level_values('gene_id')[0])
+    out.set_index(['gene_id', 'PC'], inplace=True)
+    return out
+
+def gene_fit_transform(df: pd.DataFrame, regions: pd.DataFrame, var_expl: float, n_pcs_max: int) -> PCA:
+    """PCA-transform normalized coverage for one gene.
+    
+    Saves enough PCs to explain var_expl variance or n_pcs_max, whichever is smaller.
+    """
+    df = normalize_gene(df, regions)
+    df = df.loc[df.std(axis=1) > 0, :]
+    if df.shape[0] == 0:
+        return None
+    x = df.values.T
+    # Center and scale the data:
+    x = (x - x.mean(axis=0)) / x.std(axis=0)
+    pca = PCA(n_components=var_expl) if var_expl not in {0, 1} else PCA()
+    mat = pca.fit_transform(x).T
+    if n_pcs_max != 0 and n_pcs_max < mat.shape[0]:
+        mat = mat[:n_pcs_max, :]
+    pc_names = [f'PC{i + 1}' for i in range(mat.shape[0])]
+    out = pd.DataFrame(mat, index=pc_names, columns=df.columns)
+    out.index.set_names('PC', inplace=True)
+    # Add gene_id to index:
+    out.reset_index(inplace=True)
+    out.insert(0, 'gene_id', df.index.get_level_values('gene_id')[0])
+    out.set_index(['gene_id', 'PC'], inplace=True)
+    return out
+
+def fit(df: pd.DataFrame, regions: pd.DataFrame, var_expl_max: float, n_pcs_max: int) -> dict:
+    """Fit PCA models for all genes"""
+    models = {'var_expl_max': var_expl_max, 'n_pcs_max': n_pcs_max, 'models': {}}
+    regions = regions.groupby('gene_id')
+    n_genes = df.index.get_level_values('gene_id').nunique()
+    for gene_id, x in tqdm(df.groupby('gene_id'), total=n_genes, desc="Fitting models"):
+        reg = regions.get_group(gene_id)
+        model = gene_fit(x, reg, var_expl_max, n_pcs_max)
+        if model is not None:
+            models['models'][gene_id] = model
+    return models
+
+def transform(df: pd.DataFrame, regions: pd.DataFrame, models: dict) -> pd.DataFrame:
+    """Apply PCA transformation to all genes"""
+    regions = regions.groupby('gene_id')
+    out = []
+    n_genes = df.index.get_level_values('gene_id').nunique()
+    for gene_id, x in tqdm(df.groupby('gene_id'), total=n_genes, desc="Applying transformation"):
+        if gene_id in models['models']:
+            pca = models['models'][gene_id]['pca']
+            features = models['models'][gene_id]['features']
+            reg = regions.get_group(gene_id)
+            out.append(gene_transform(x, reg, pca, features))
+    out = pd.concat(out)
+    out = out.reset_index()
+    return out
+
+def fit_transform(df: pd.DataFrame, regions: pd.DataFrame, var_expl_max: float, n_pcs_max: int) -> pd.DataFrame:
+    """Fit and transform each gene one by one so not all models are stored in memory at once"""
+    regions = regions.groupby('gene_id')
+    out = []
+    n_genes = df.index.get_level_values('gene_id').nunique()
+    for gene_id, x in tqdm(df.groupby('gene_id'), total=n_genes, desc="Fitting and transforming"):
+        reg = regions.get_group(gene_id)
+        out.append(gene_fit_transform(x, reg, var_expl_max, n_pcs_max))
+    out = pd.concat(out)
+    out = out.reset_index()
+    return out
+
+def create_parser():
+    parser = argparse.ArgumentParser(description='Fit and/or apply PCA model on feature bin coverage data')
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand', required=True, help='Choose a subcommand')
+
+    # Subparser for 'fit'
+    parser_fit = subparsers.add_parser('fit', help='Fit PCA model')
+    parser_fit.add_argument('-i', '--inputs', type=Path, metavar='FILE', required=True, help='File containing paths to all input coverage files.')
+    parser_fit.add_argument('-r', '--regions', type=Path, metavar='FILE', required=True, help='BED file containing regions to use for PCA. Must have start, end and region ID in 2nd, 3rd, and 4th columns. Rows must correspond to rows of input coverage files.')
+    parser_fit.add_argument('-v', '--var-expl-max', type=float, default=0.8, metavar='FLOAT', help='Max variance explained by the PCs kept per gene. Pass 0 or 1 for no variance explained cutoff. Default 0.8.')
+    parser_fit.add_argument('-n', '--n-pcs-max', type=int, default=32, metavar='N', help='Max number of PCs to keep per gene. Pass 0 for no cutoff. Default 32.')
+    parser_fit.add_argument('-o', '--output', type=Path, metavar='FILE', required=True, help='Output file (*.pickle) to save PCA models')
+
+    # Subparser for 'transform'
+    parser_transform = subparsers.add_parser('transform', help='Apply PCA transformation')
+    parser_transform.add_argument('-i', '--inputs', type=Path, metavar='FILE', required=True, help='File containing paths to all input coverage files. Base file name before first "." is sample ID.')
+    parser_transform.add_argument('-r', '--regions', type=Path, metavar='FILE', required=True, help='BED file containing regions to use for PCA. Must have start, end and region ID in 2nd, 3rd, and 4th columns. Rows must correspond to rows of input coverage files.')
+    parser_transform.add_argument('-m', '--models', type=Path, metavar='FILE', required=True, help='PCA models file (*.pickle) to load and use for transformation')
+    parser_transform.add_argument('-o', '--output', type=Path, metavar='FILE', required=True, help='Output file (TSV)')
+
+    # Subparser for 'fit-transform'
+    parser_fit_transform = subparsers.add_parser('fit-transform', help='Fit PCA model and apply transformation')
+    parser_fit_transform.add_argument('-i', '--inputs', type=Path, metavar='FILE', required=True, help='File containing paths to all input coverage files. Base file name before first "." is sample ID.')
+    parser_fit_transform.add_argument('-r', '--regions', type=Path, metavar='FILE', required=True, help='BED file containing regions to use for PCA. Must have start, end and region ID in 2nd, 3rd, and 4th columns. Rows must correspond to rows of input coverage files.')
+    parser_fit_transform.add_argument('-v', '--var-expl-max', type=float, default=0.8, metavar='FLOAT', help='Max variance explained by the PCs kept per gene. Pass 0 or 1 for no variance explained cutoff. Default 0.8.')
+    parser_fit_transform.add_argument('-n', '--n-pcs-max', type=int, default=32, metavar='N', help='Max number of PCs to keep per gene. Pass 0 for no cutoff. Default 32.')
+    parser_fit_transform.add_argument('-o', '--output', type=Path, metavar='FILE', required=True, help='Output file (TSV)')
+
+    return parser
+
+if __name__ == '__main__':
+    parser = create_parser()
+    args = parser.parse_args()
+    regions = load_regions(args.regions)
+    inputs = load_inputs(args.inputs, regions, parse_ids=('transform' in args.subcommand))
+    if args.subcommand == 'fit':
+        print('Fitting PCA models...', flush=True)
+        models = fit(inputs, regions, args.var_expl_max, args.n_pcs_max)
+        with open(args.output, 'wb') as f:
+            pickle.dump(models, f)
+        print(f'PCA models saved to {args.output}', flush=True)
+    elif args.subcommand == 'transform':
+        print('Loading PCA models...', flush=True)
+        with open(args.models, 'rb') as f:
+            models = pickle.load(f)
+        print('Applying PCA transformation...', flush=True)
+        output = transform(inputs, regions, models)
+        output.to_csv(args.output, sep='\t', index=False, float_format='%g')
+        print(f'PCA-transformed features saved to {args.output}', flush=True)
+    elif args.subcommand == 'fit-transform':
+        print('Fitting PCA models and applying transformation...', flush=True)
+        output = fit_transform(inputs, regions, args.var_expl_max, args.n_pcs_max)
+        output.to_csv(args.output, sep='\t', index=False, float_format='%g')
+        print(f'PCA-transformed features saved to {args.output}', flush=True)
