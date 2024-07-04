0a220e6732ddce648b1fa22e3c9ffc1c72818989
Daniel Munro <danbmunro@gmail.com>
2024-07-03 20:35:02 -0500

    Add functional PCA

--- latent_RNA.py
+++ latent_RNA.py
@@ -4,9 +4,13 @@ import argparse
 import math
 from pathlib import Path
 import pickle
+from typing import Iterator
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import PCA
+from skfda.preprocessing.dim_reduction import FPCA
+from skfda.representation.basis import BSplineBasis
+from skfda.representation.grid import FDataGrid
 from sklearn.linear_model import LinearRegression
 from tqdm import tqdm
 
@@ -32,9 +36,77 @@ class CoverageData:
         df = pd.DataFrame(mat, index=regions.index, columns=self.samples)
         return df, regions
 
-    def by_gene(self):
+    def by_gene(self) -> Iterator[tuple]:
         for gene_id, df in self.counts.groupby('gene_id'):
             yield gene_id, df
+
+class Model:
+    def __init__(self, fpca):
+        self.model = None
+        self.features = None
+        self.fpca = fpca
+
+    def fit(self, df: pd.DataFrame, var_expl: float, n_pcs_max: int):
+        """Fit functional PCA model to normalized coverage for one gene
+        
+        Saves enough PCs to explain var_expl variance or n_pcs_max, whichever is smaller.
+        """
+        df = df.loc[df.std(axis=1) > 0, :]
+        if df.shape[0] == 0:
+            return
+        x = df.values.T
+        # # Center and scale the data:
+        xmean = x.mean(axis=0)
+        xstd = x.std(axis=0)
+        x = (x - xmean) / xstd
+        if self.fpca:
+            fd = FDataGrid(x, grid_points=df.index.get_level_values('start'))
+            # FPCA currently defaults to n_components=3, so we need to set it explicitly:
+            # (There's no option for variance explained cutoff, so we'll subset the PCs later.)
+            n_comp = min(n_pcs_max, x.shape[0], x.shape[1])
+            basis_fd = fd.to_basis(BSplineBasis(n_basis=n_comp, order=4))
+            # Must pass weights to avoid 'Matrix is not positive definite' error:
+            model = FPCA(n_components=n_comp, centering=False, _weights=np.ones(x.shape[1]))
+            model.fit(basis_fd)
+            subset_pcs_fpca(model, var_expl)
+        else:
+            model = PCA(n_components=var_expl) if var_expl not in {0, 1} else PCA()
+            model.fit(x)
+            subset_pcs_pca(model, n_pcs_max)
+        self.model = model
+        self.features = pd.DataFrame(index=df.index) # Save feature stats for transforming new data
+        self.features['mean'] = xmean
+        self.features['std'] = xstd
+
+    def transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Apply functional PCA model to normalized coverage for one gene"""
+        # Filter df to include same features as the model:
+        # print(df)
+        # print(self.features)
+        # assert False
+        df = df.loc[self.features.index, :]
+        x = df.values.T
+        # Center and scale the data using stats from the model-fitting data:
+        x = (x - self.features['mean'].values) / self.features['std'].values
+        if self.fpca:
+            fd = FDataGrid(x, grid_points=df.index.get_level_values('start'))
+            # print(fd)
+            # assert False
+            n_basis = self.model.components_.n_basis
+            basis_fd = fd.to_basis(BSplineBasis(n_basis=n_basis, order=4))
+            mat = self.model.transform(basis_fd).T
+            # print(mat)
+            # assert False
+        else:
+            mat = self.model.transform(x).T
+        pc_names = [f'PC{i + 1}' for i in range(mat.shape[0])]
+        out = pd.DataFrame(mat, index=pc_names, columns=df.columns)
+        out.index.set_names('PC', inplace=True)
+        # Add gene_id to index:
+        out.reset_index(inplace=True)
+        out.insert(0, 'gene_id', df.index.get_level_values('gene_id')[0])
+        out.set_index(['gene_id', 'PC'], inplace=True)
+        return out
 
 def load_regions(regionfile: Path) -> pd.DataFrame:
     regions = pd.read_csv(regionfile, sep='\t', header=None, usecols=[1, 2, 3], names=['start', 'end', 'region'], index_col='region')
@@ -78,10 +150,10 @@ def normalize_coverage(df: pd.DataFrame, regions: pd.DataFrame) -> pd.DataFrame:
     """Normalize coverage for each gene in one batch"""
     # Normalize coverage to ignore total expression, i.e. total coverage per sample per gene sums to 1:
     df = df.groupby('gene_id', group_keys=False).apply(norm_counts_over_gene)
-    # Normalize counts to mean coverage per bp within each region:
+    # Normalize to mean read fraction per bp within each region:
     df = df.div(regions['length'].loc[df.index], axis=0)
     # Variance stabilization:
-    df = np.sqrt(df)
+    df = np.log2(df + 5)
     return df
 
 def load_phenotypes(pheno_file: Path, samples: list) -> pd.DataFrame:
@@ -151,7 +223,19 @@ def prepare(infile: Path, regionfile: Path, pheno_files: list, batch_size: int, 
         reg = regions.loc[x.index, :]
         reg.to_csv(outdir / f'covg_{batch_id}.regions.tsv.gz', sep='\t')
 
-def subset_pcs(pca: PCA, n_pcs_max: int):
+def subset_pcs_fpca(fpca: FPCA, var_expl: float):
+    """Subset the FPCA model using variance explained cutoff"""
+    # Find the number of PCs needed to explain var_expl variance:
+    n_pcs_max = np.argmax(np.cumsum(fpca.explained_variance_ratio_) >= var_expl) + 1
+    # if n_pcs_max < fpca.components_.shape[0]:
+    #     fpca.components_.data_matrix = fpca.components_.data_matrix[:n_pcs_max, :]
+    if n_pcs_max < fpca.components_.coefficients.shape[0]:
+        fpca.components_.coefficients = fpca.components_.coefficients[:n_pcs_max, :]
+        fpca.explained_variance_ = fpca.explained_variance_[:n_pcs_max]
+        fpca.explained_variance_ratio_ = fpca.explained_variance_ratio_[:n_pcs_max]
+        fpca.singular_values_ = fpca.singular_values_[:n_pcs_max]
+
+def subset_pcs_pca(pca: PCA, n_pcs_max: int):
     """Subset the PCA model to n_pcs_max PCs to reduce file size"""
     if n_pcs_max != 0 and n_pcs_max < pca.n_components_:
         pca.components_ = pca.components_[:n_pcs_max, :]
@@ -161,58 +245,19 @@ def subset_pcs(pca: PCA, n_pcs_max: int):
         pca.singular_values_ = pca.singular_values_[:n_pcs_max]
         pca.noise_variance_ = None # Could compute if necessary, but omitting otherwise
 
-def fit_gene(df: pd.DataFrame, n_samples_max: int, var_expl: float, n_pcs_max: int) -> PCA:
-    """Fit PCA model to normalized coverage for one gene
-    
-    Saves enough PCs to explain var_expl variance or n_pcs_max, whichever is smaller.
-    """
-    if n_samples_max != 0 and n_samples_max < df.shape[1]:
-        df = df.sample(n=n_samples_max, axis=1)
-    df = df.loc[df.std(axis=1) > 0, :]
-    if df.shape[0] == 0:
-        return None
-    x = df.values.T
-    # Center and scale the data:
-    xmean = x.mean(axis=0)
-    xstd = x.std(axis=0)
-    x = (x - xmean) / xstd
-    pca = PCA(n_components=var_expl) if var_expl not in {0, 1} else PCA()
-    pca.fit(x)
-    subset_pcs(pca, n_pcs_max)
-    features = pd.DataFrame(index=df.index) # Save feature stats for transforming new data
-    features['mean'] = xmean
-    features['std'] = xstd
-    return {'pca': pca, 'features': features}
-
-def transform_gene(df: pd.DataFrame, pca: PCA, features: pd.DataFrame) -> pd.DataFrame:
-    """Apply PCA model to normalized coverage for one gene"""
-    # Filter df to include same features as the model:
-    df = df.loc[features.index, :]
-    x = df.values.T
-    # Center and scale the data using stats from the model-fitting data:
-    x = (x - features['mean'].values) / features['std'].values
-    mat = pca.transform(x).T
-    pc_names = [f'PC{i + 1}' for i in range(mat.shape[0])]
-    out = pd.DataFrame(mat, index=pc_names, columns=df.columns)
-    out.index.set_names('PC', inplace=True)
-    # Add gene_id to index:
-    out.reset_index(inplace=True)
-    out.insert(0, 'gene_id', df.index.get_level_values('gene_id')[0])
-    out.set_index(['gene_id', 'PC'], inplace=True)
-    return out
-
-def fit_batch(batch_covg_dirs: list, batch: int, n_samples_max: int, var_expl_max: float, n_pcs_max: int) -> dict:
-    """Fit PCA models for all genes in a batch"""
+def fit_batch(batch_covg_dirs: list, batch: int, var_expl_max: float, n_pcs_max: int, fpca: bool = True) -> dict:
+    """Fit functional PCA models for all genes in a batch"""
     covg = CoverageData(batch_covg_dirs, batch)
     models = {'var_expl_max': var_expl_max, 'n_pcs_max': n_pcs_max, 'models': {}}
     for gene_id, x in tqdm(covg.by_gene(), total=len(covg.genes), desc="Fitting models"):
-        model = fit_gene(x, n_samples_max, var_expl_max, n_pcs_max)
-        if model is not None:
+        model = Model(fpca)
+        model.fit(x, var_expl_max, n_pcs_max)
+        if model.model is not None:
             models['models'][gene_id] = model
     return models
 
-def fit(batch_covg_dirs: list, batch_id: int, n_samples_max: int, var_expl_max: float, n_pcs_max: int, output_dir: Path) -> dict:
-    """Fit PCA models for all genes in one or more batches"""
+def fit(batch_covg_dirs: list, batch_id: int, var_expl_max: float, n_pcs_max: int, output_dir: Path, fpca: bool = True):
+    """Fit functional PCA models for all genes in one or more batches"""
     if batch_id is None:
         with open(batch_covg_dirs[0] / 'batches.txt') as f:
             batches = [int(l.strip()) for l in f]
@@ -221,14 +266,14 @@ def fit(batch_covg_dirs: list, batch_id: int, n_samples_max: int, var_expl_max: 
     output_dir.mkdir(exist_ok=True)
     for batch in batches:
         print(f'=== Fitting models for batch {batch} ===', flush=True)
-        models = fit_batch(batch_covg_dirs, batch, n_samples_max, var_expl_max, n_pcs_max)
+        models = fit_batch(batch_covg_dirs, batch, var_expl_max, n_pcs_max, fpca)
         outfile = output_dir / f'models_{batch}.pickle'
         with open(outfile, 'wb') as f:
             pickle.dump(models, f)
         print(f'Models saved to {outfile}', flush=True)
 
-def transform_batch(batch_covg_dir: Path, batch: int, models_dir: dict) -> pd.DataFrame:
-    """Apply PCA transformation to all genes in a batch"""
+def transform_batch(batch_covg_dir: Path, batch: int, models_dir: dict) -> Iterator[pd.DataFrame]:
+    """Apply functional PCA transformation to all genes in a batch"""
     covg = CoverageData([batch_covg_dir], batch)
     print('  Loading models...', flush=True)
     models_file = models_dir / f'models_{batch}.pickle'
@@ -236,12 +281,10 @@ def transform_batch(batch_covg_dir: Path, batch: int, models_dir: dict) -> pd.Da
         models = pickle.load(f)
     for gene_id, x in tqdm(covg.by_gene(), total=len(covg.genes), desc="  Generating phenotypes per gene"):
         if gene_id in models['models']:
-            pca = models['models'][gene_id]['pca']
-            features = models['models'][gene_id]['features']
-            yield transform_gene(x, pca, features)
+            yield models['models'][gene_id].transform(x)
 
 def transform(batch_covg_dir: Path, models_dir: dict) -> pd.DataFrame:
-    """Apply PCA transformation to all genes"""
+    """Apply functional PCA transformation to all genes"""
     with open(batch_covg_dir / 'batches.txt') as f:
         batches = [int(l.strip()) for l in f]
     out = []
@@ -272,16 +315,16 @@ def create_parser():
     parser_fit_input.add_argument('-d', '--batch-covg-dir', nargs='+', type=Path, metavar='DIR', help='Directory of per-batch numpy binary files. Specify multiple directories to load data from all of them and fit models using the combined dataset. All datasets must have been generated using the same gene bins file.')
     parser_fit_input.add_argument('--dir-file', type=Path, metavar='FILE', help='File containing list of directories of per-batch numpy binary files. Use this instead of -d/--gene-covg-dir in case of many directories.')
     parser_fit.add_argument('-b', '--batch', type=int, metavar='N', help='Batch number to load and fit. Batch numbers start from 0. If omitted, all batches will be loaded and fit in sequence.')
-    parser_fit.add_argument('-m', '--models-dir', type=Path, metavar='DIR', required=True, help='Directory to save PCA model pickle files')
-    parser_fit.add_argument('--n-samples-max', type=int, default=0, metavar='N', help='Max number of samples to use for fitting PCA models faster. Loaded samples are randomly subsetted if higher than this, done after loading and concatenating data from multiple datasets if applicable, and the sample subsets are chosen independently per gene. Pass 0 for no cutoff.')
+    parser_fit.add_argument('-m', '--models-dir', type=Path, metavar='DIR', required=True, help='Directory in which to save PCA model pickle files.')
     parser_fit.add_argument('-v', '--var-expl-max', type=float, default=0.8, metavar='FLOAT', help='Max variance explained by the PCs kept per gene. Pass 0 or 1 for no variance explained cutoff. Default 0.8.')
     parser_fit.add_argument('-n', '--n-pcs-max', type=int, default=32, metavar='N', help='Max number of PCs to keep per gene. Pass 0 for no cutoff. Default 32.')
+    parser_fit.add_argument('--regular-pca', action='store_true', help='Use regular PCA instead of functional PCA.')
 
     # Subparser for 'transform'
     parser_transform = subparsers.add_parser('transform', help='Apply PCA transformation')
     parser_transform.add_argument('-d', '--batch-covg-dir', type=Path, metavar='DIR', required=True, help='Directory of per-batch numpy binary files.')
-    parser_transform.add_argument('-m', '--models-dir', type=Path, metavar='DIR', required=True, help='Directory of saved PCA models (*.pickle) to load and use for transformation')
-    parser_transform.add_argument('-o', '--output', type=Path, metavar='FILE', required=True, help='Output file (TSV)')
+    parser_transform.add_argument('-m', '--models-dir', type=Path, metavar='DIR', required=True, help='Directory of saved PCA models (*.pickle) to load and use for transformation.')
+    parser_transform.add_argument('-o', '--output', type=Path, metavar='FILE', required=True, help='Output file (TSV).')
 
     return parser
 
@@ -305,7 +348,7 @@ if __name__ == '__main__':
         else:
             with open(args.dir_file) as f:
                 batch_covg_dirs = [Path(l.strip()) for l in f]
-        fit(batch_covg_dirs, args.batch, args.n_samples_max, args.var_expl_max, args.n_pcs_max, args.models_dir)
+        fit(batch_covg_dirs, args.batch, args.var_expl_max, args.n_pcs_max, args.models_dir, not args.regular_pca)
     elif args.subcommand == 'transform':
         print('=== Generating latent phenotypes ===', flush=True)
         output = transform(args.batch_covg_dir, args.models_dir)
